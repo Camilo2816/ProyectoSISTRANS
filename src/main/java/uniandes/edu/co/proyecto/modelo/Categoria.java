236cf2a668533cb871cd4513b0dfd83ff7d287d3
UsuarioGithub <correo@github.com>
2024-09-28 16:34:04 -0500

    c y p

---
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Categoria.java
@@ -0,0 +1,53 @@
+package uniandes.edu.co.proyecto.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private String caracteristicasAlmacenamiento;
+    
+    public Categoria(String nombre, String descripcion, String caracteristicasAlmacenamiento){
+
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.caracteristicasAlmacenamiento = caracteristicasAlmacenamiento;
+    } 
+    public Categoria()
+    {;}
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getCaracteristicasAlmacenamiento() {
+        return caracteristicasAlmacenamiento;
+    }
+    public void setCaracteristicasAlmacenamiento(String caracteristicasAlmacenamiento) {
+        this.caracteristicasAlmacenamiento = caracteristicasAlmacenamiento;
+    }
+    
+}
