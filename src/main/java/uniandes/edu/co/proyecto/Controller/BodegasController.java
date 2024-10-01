f3fffbc79823676c4dfd9c08c8bf1e3bace3abb6
UsuarioGithub <correo@github.com>
2024-09-30 21:10:09 -0500

    45487

--- src/main/java/uniandes/edu/co/proyecto/Controller/BodegasController.java
+++ src/main/java/uniandes/edu/co/proyecto/Controller/BodegasController.java
@@ -1,16 +1,25 @@
 package uniandes.edu.co.proyecto.Controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
+import java.util.HashMap;
+
 import uniandes.edu.co.proyecto.modelo.Bodega;
 import uniandes.edu.co.proyecto.repositorio.BodegaRepository;
+import uniandes.edu.co.proyecto.repositorio.BodegaRepository.RespuestaInformacionBodegas;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class BodegasController {
@@ -22,6 +31,31 @@ public class BodegasController {
     public Collection<Bodega> bodegas() {
         return bodegaRepository.darBodegas();
     }
+
+    @GetMapping("/bodegas/consulta")
+    public ResponseEntity<?> bodegasConsulta(@RequestParam(required = false) Integer sucursal_id,
+                                                @RequestParam(required = false) List<Integer> lista_procuctos){
+    try {
+        Collection<RespuestaInformacionBodegas> informacion = bodegaRepository.darInformacionBodegas();
+        RespuestaInformacionBodegas info = informacion.iterator().next();
+        Map<String, Object> response = new HashMap<>();
+        response.put("nombreBodega", info.getNOMBRE_BODEGA());
+        response.put("nombreSucursal", info.getNOMBRE_SUCURSAL());
+        response.put("porcentajeOcupacion", info.getPORCENTAJE_OCUPACION());
+
+        Collection<Bodega> bodegas;
+        if ((sucursal_id == null ) || (lista_procuctos.isEmpty())){
+        bodegas = bodegaRepository.darBodegas();
+        } else {
+            bodegas = bodegaRepository.darBodegasOcupacion(sucursal_id, lista_procuctos);
+        }
+        response.put("bodegas", bodegas);                                    
+        return ResponseEntity.ok(response);
+    } catch (Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+                                            }
+    
     
     @PostMapping("/bodegas/new/save")
     public ResponseEntity<String> bodegaGuardar(@RequestBody Bodega bodega) {
--- src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
+++ src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
@@ -1,40 +1,14 @@
 package uniandes.edu.co.proyecto;
-
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import uniandes.edu.co.proyecto.repositorio.BodegaRepository;
-import uniandes.edu.co.proyecto.repositorio.CategoriaRepository;
-import uniandes.edu.co.proyecto.repositorio.CiudadRepository;
-import uniandes.edu.co.proyecto.repositorio.SucursalRepository;
-import uniandes.edu.co.proyecto.modelo.*;;
-
 
 @SpringBootApplication
-public class ProyectoApplication  implements CommandLineRunner{
+public class ProyectoApplication {
 
 
-	@Autowired
-	private BodegaRepository ciudadRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
-	}
-
-
-	@Override
-	public void run(String... args)  
-	 {
-
-		Collection<Bodega> ciudad = ciudadRepository.darBodegas();
-		
-		for (Bodega bodega : ciudad) {
-			System.out.println(bodega.getNombre());
-		}
-
 	}
 }
